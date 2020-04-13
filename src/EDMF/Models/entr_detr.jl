bb7b63323bdd75b832fb3040c97b621c58a4a30f
yairchn <yair.chn@gmail.com>
2020-04-12 20:12:36 -0700

    ..

--- src/EDMF/Models/entr_detr.jl
+++ src/EDMF/Models/entr_detr.jl
@@ -5,6 +5,7 @@ abstract type EntrDetrModel end
 struct BOverW2{FT} <: EntrDetrModel
   ε_factor::FT
   δ_factor::FT
+  δ_power::FT
 end
 
 """
@@ -19,7 +20,8 @@ Define entrainment and detrainment fields
 """
 function compute_entrainment_detrainment! end
 
-function compute_entrainment_detrainment!(grid::Grid{FT}, UpdVar, tmp, q, params, model::BOverW2) where FT
+"""
+function compute_entrainment_detrainment_bw2!(grid::Grid{FT}, UpdVar, tmp, q, params, model::BOverW2) where FT
   gm, en, ud, sd, al = allcombinations(q)
   Δzi = grid.Δzi
   k_1 = first_interior(grid, Zmin())
@@ -41,8 +43,43 @@ function compute_entrainment_detrainment!(grid::Grid{FT}, UpdVar, tmp, q, params
     tmp[:δ_model, k_1, i] = FT(0)
   end
 end
+"""
 
-
+function compute_entrainment_detrainment!(grid::Grid{FT}, UpdVar, tmp, q, params, model::BOverW2) where FT
+  gm, en, ud, sd, al = allcombinations(q)
+  Δzi = grid.Δzi
+  k_1 = first_interior(grid, Zmin())
+  beta = model.δ_power
+  c_ent = model.ε_factor
+  c_det = model.δ_factor
+  @inbounds for k in over_elems_real(grid)
+    RH_en = relative_humidity(q, tmp, k, en)
+    w_env = q[:w, k, en]
+    b_env = tmp[:buoy, k, env]
+    @inbounds for i in ud
+      db = tmp[:buoy, k, i] - b_env
+      w_up = q[:w, k, i]
+      dw = w_up - w_env
+      RH_up = relative_humidity(q, tmp, k, i)
+      if dw < 0.0
+        dw = dw -  0.001
+      else
+        dw = dw + 0.001
+      end
+      if ([:ql, k, i]+[:ql, k, en])==0.0
+        c_det = 0.0
+      else
+        c_det = 1.0
+      end
+      moisture_deficit_d = (max(RH_upd^2.0-RH_env^2.0,0.0))^(1.0/2.0)
+      moisture_deficit_e = (max(RH_env^2.0-RH_upd^2.0,0.0))^(1.0/2.0)
+      tmp[:ε_model, k, i] = abs(db/dw)/max(w_up*w_up,1e-4)*(c_ent*logistic_e+c_det*moisture_deficit_e)
+      tmp[:δ_model, k, i] = abs(db/dw)/max(w_up*w_up,1e-4)*(c_ent*logistic_d+c_det*moisture_deficit_e)
+    end
+    tmp[:ε_model, k_1, i] = 2 * Δzi
+    tmp[:δ_model, k_1, i] = FT(0)
+  end
+end
 
 function compute_cv_entr!(grid::Grid{FT}, q, tmp, tmp_O2, ϕ, ψ, cv, tke_factor) where FT
   gm, en, ud, sd, al = allcombinations(q)
