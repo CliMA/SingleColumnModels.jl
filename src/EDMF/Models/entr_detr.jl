91021a33e1cb88623bf9bb5f8c7dd7b174bb6756
yairchn <yair.chn@gmail.com>
2020-04-12 20:58:46 -0700

    add toml

--- src/EDMF/Models/entr_detr.jl
+++ src/EDMF/Models/entr_detr.jl
@@ -20,31 +20,6 @@ Define entrainment and detrainment fields
 """
 function compute_entrainment_detrainment! end
 
-"""
-function compute_entrainment_detrainment_bw2!(grid::Grid{FT}, UpdVar, tmp, q, params, model::BOverW2) where FT
-  gm, en, ud, sd, al = allcombinations(q)
-  Δzi = grid.Δzi
-  k_1 = first_interior(grid, Zmin())
-  @inbounds for i in ud
-    zi = UpdVar[i].cloud.base
-    @inbounds for k in over_elems_real(grid)
-      buoy = tmp[:buoy, k, i]
-      w = q[:w, k, i]
-      if grid.zc[k] >= zi
-        detr_sc = 4.0e-3 + 0.12 *abs(min(buoy,0.0)) / max(w * w, 1e-2)
-      else
-        detr_sc = FT(0)
-      end
-      entr_sc = 0.12 * max(buoy, FT(0) ) / max(w * w, 1e-2)
-      tmp[:ε_model, k, i] = entr_sc * model.ε_factor
-      tmp[:δ_model, k, i] = detr_sc * model.δ_factor
-    end
-    tmp[:ε_model, k_1, i] = 2 * Δzi
-    tmp[:δ_model, k_1, i] = FT(0)
-  end
-end
-"""
-
 function compute_entrainment_detrainment!(grid::Grid{FT}, UpdVar, tmp, q, params, model::BOverW2) where FT
   gm, en, ud, sd, al = allcombinations(q)
   Δzi = grid.Δzi
