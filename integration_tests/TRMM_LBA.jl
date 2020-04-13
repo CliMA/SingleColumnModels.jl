ab2c2e5e89f32c358ae7687fb83147d8fbe62cdb
yairchn <yairchn@caltech.edu>
2020-04-13 16:44:10 -0700

    Add TRMM_LBA

---
+++ integration_tests/TRMM_LBA.jl
@@ -0,0 +1,71 @@
+if !("." in LOAD_PATH) # for easier local testing
+    push!(LOAD_PATH, ".")
+end
+using Test
+using SingleColumnModels
+using OrderedCollections
+using SingleColumnModels.StateVecs
+const scm = SingleColumnModels
+using CLIMAParameters
+using CLIMAParameters.Planet
+import CLIMAParameters.Planet
+
+# TODO: remove this hack!
+import Thermodynamics
+Thermodynamics.error_on_non_convergence() = false
+
+ENV["GKSwstype"] = "nul"
+
+const scm_dir = dirname(dirname(pathof(SingleColumnModels)));
+output_dir = joinpath(scm_dir, "output", "TRMM_LBA")
+mkpath(output_dir)
+const pycles_comparison = joinpath(output_dir, "pycles_comparison")
+const plot_dir = joinpath(output_dir, "plots")
+
+struct EarthParameterSet <: AbstractEarthParameterSet end
+CLIMAParameters.Planet.MSLP(::EarthParameterSet) = 100000.0
+const param_set = EarthParameterSet()
+
+best_mse = OrderedDict()
+best_mse[:q_tot_gm] = 6.7658511077496641e-01
+best_mse[:a_up] = 1.1223097298480937e+02
+best_mse[:w_up] = 6.6796928164464944e+01
+best_mse[:q_tot_up] = 1.4306494455000141e+01
+best_mse[:θ_liq_up] = 1.0030819699496138e+02
+best_mse[:tke_en] = 9.4861001071352035e+01
+
+# Define `compute_mse` and retrieve data file:
+include(joinpath(@__DIR__, "utils", "compute_mse.jl"))
+include(joinpath(@__DIR__, "utils", "plot_profiles.jl"))
+data_file = Dataset(joinpath(PyCLES_output_dataset_path, "TRMM_LBA.nc"), "r")
+
+@testset "TRMM_LBA EDMF Solution Quality Assurance (QA) tests" begin
+
+    grid, q, aux, params =
+        scm.EDMF.run(param_set, scm.EDMF.TRMM_LBA(), output_dir)
+    computed_mse = compute_mse(
+        grid,
+        q,
+        aux,
+        params,
+        data_file,
+        "TRMM_LBA",
+        best_mse,
+        21600.0,
+        pycles_comparison,
+    )
+
+    # Export plots (must export before tests):
+    skip_fields = ["a_gm", "u_en", "u_ud_1", "v_en", "v_gm", "v_ud_1", "w_gm"]
+    prog_fn = joinpath(output_dir, "prog_vs_time.nc")
+    plot_profiles(prog_fn, plot_dir; skip_fields = skip_fields)
+    # aux_fn = joinpath(output_dir, "aux_vs_time.nc")
+    # plot_profiles(aux_fn, plot_dir)
+
+    test_mse(computed_mse, best_mse, :q_tot_gm)
+    test_mse(computed_mse, best_mse, :a_up)
+    test_mse(computed_mse, best_mse, :w_up)
+    test_mse(computed_mse, best_mse, :q_tot_up)
+    test_mse(computed_mse, best_mse, :θ_liq_up)
+    test_mse(computed_mse, best_mse, :tke_en)
+end
--- src/EDMF/init_params.jl
+++ src/EDMF/init_params.jl
@@ -132,3 +132,127 @@ function Params(param_set, ::BOMEX)
 
     return params
 end
+
+function Params(param_set, ::TRMM_LBA)
+    FT = Float64
+    IT = Int
+    params = Dict{Symbol, Any}()
+
+    #####
+    ##### TODO: Parameters that need to be added to CLIMAParameters
+    #####
+    params[:k_Karman] = 0.4 # "Von Karman constant (unit-less)"
+
+    #####
+    ##### Filter parameters
+    #####
+    params[:param_set] = param_set
+    params[:N_subdomains] = 3
+
+    #####
+    ##### IO parameters
+    #####
+
+    params[:export_data] = false
+    params[:plot_single_fields] = true
+    params[:export_frequency] = 2000
+
+
+    #####
+    ##### Filter parameters
+    #####
+
+    params[:a_bounds] = [1e-3, 1 - 1e-3]                                  # filter for a
+    params[:w_bounds] = [0.0, 10000.0]                                  # filter for w
+    params[:q_bounds] = [0.0, 1.0]                                      # filter for q
+
+    #####
+    ##### Space and time discretization
+    #####
+
+    params[:UniformGridParams] =
+        UniformGridParams{FT, IT}(; z_min = 0.0, z_max = 16000.0, n_elems = 320)
+    grid_params = params[:UniformGridParams]
+    params[:Δz] = (grid_params.z_max - grid_params.z_min) / grid_params.n_elems
+
+    params[:Δt] = FT(30.0)
+    params[:Δt_min] = FT(30.0)
+    params[:t_end] = FT(21600.0)
+    params[:CFL] = FT(0.8)
+
+    #####
+    ##### External conditions
+    #####
+
+    # Looks good
+    params[:ForcingType] = NoForcing()
+
+    # Looks okay
+    # params[:ForcingType] = StandardForcing(apply_subsidence=true,
+    #                                        apply_coriolis=true,
+    #                                        coriolis_param=FT(0.376e-4))
+    #####
+    ##### Physical models
+    #####
+
+    params[:EntrDetrModel] = BOverW2{FT}(1, 1)
+    # Looks okay
+    params[:MixingLengthModel] = ConstantMixingLength{FT}(100)
+    # Looks okay
+    # params[:MixingLengthModel]      = SCAMPyMixingLength{FT}(StabilityDependentParam{FT}(2.7,-100.0),
+    #                                                          StabilityDependentParam{FT}(-1.0,-0.2))
+
+    # Getting NaNs for TKE and other fields. Something needs to be fixed
+    # params[:MixingLengthModel]      = IgnaciosMixingLength(StabilityDependentParam{FT}(2.7,-100.0),
+    #                                                        StabilityDependentParam{FT}(-1.0,-0.2),
+    #                                                        0.1, 0.12, 0.4, 40/13, 0.74)
+
+    params[:EddyDiffusivityModel] = SCAMPyEddyDiffusivity{FT}(0.1)
+
+    params[:PressureModel] = SCAMPyPressure{FT}(;
+        buoy_coeff = FT(1.0 / 3.0),
+        drag_coeff = FT(0.375),
+        plume_spacing = FT(2000.0),
+    )
+    params[:prandtl_number] = FT(1.0)
+    params[:tke_diss_coeff] = FT(2.0)
+
+    params[:f_coriolis] = 0.376e-4                                      # coriolis force coefficient
+    params[:f_c] = 0                                                    # buoyancy gradient factor
+
+    params[:zrough] = 1.0e-4                                            # surface roughness
+
+    params[:SurfaceModel] = SurfaceFixedFlux{FT}(
+        param_set;
+        T = FT(300.4),
+        P = FT(1.015e5),
+        q_tot = FT(0.02245),
+        ustar = FT(0.28),
+        windspeed_min = FT(0.0),
+        tke_tol = FT(0.01),
+        area = FT(0.1),
+    )
+
+    params[:inversion_height] = [1.0 for i in 1:params[:N_subdomains]]  # inversion height
+    params[:Ri_bulk_crit] = 0.0                                         # inversion height parameters
+    _molmass_ratio::FT = FT(molmass_ratio(param_set))
+    _grav::FT = FT(grav(param_set))
+    params[:bflux] = (
+        _grav * (
+            (
+                8.0e-3 +
+                (_molmass_ratio - 1) * (299.1 * 5.2e-5 + 22.45e-3 * 8.0e-3)
+            ) / (299.1 * (1.0 + (_molmass_ratio - 1) * 22.45e-3))
+        )
+    )
+    params[:cq] = 0.001133                                              # Some surface parameter in SCAMPy
+    params[:ch] = 0.001094                                              # Some surface parameter in SCAMPy
+    params[:cm] = 0.001229                                              # Some surface parameter in SCAMPy
+    params[:grid_adjust] =
+        (log(20.0 / params[:zrough]) / log(params[:Δz] / 2 / params[:zrough]))^2 # Some surface parameter in SCAMPy
+    params[:cq] *= params[:grid_adjust]                                 # Some surface parameter in SCAMPy
+    params[:ch] *= params[:grid_adjust]                                 # Some surface parameter in SCAMPy
+    params[:cm] *= params[:grid_adjust]                                 # Some surface parameter in SCAMPy
+
+    return params
+end
