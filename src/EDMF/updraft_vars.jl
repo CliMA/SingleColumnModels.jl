b47c410e786ba2005c74f460e4946b4e956568b2
yairchn <yairchn@caltech.edu>
2020-04-14 11:51:33 -0700

    ..

--- src/EDMF/updraft_vars.jl
+++ src/EDMF/updraft_vars.jl
@@ -9,7 +9,7 @@ mutable struct Cloud{FT}
   base::FT
   top::FT
   cover::FT
-  # updraft_top::FT
+  updraft_top::FT
 end
 
 mutable struct SurfaceBC{FT}
@@ -25,7 +25,7 @@ struct UpdraftVar{FT}
   surface_scalar_coeff::FT
   function UpdraftVar(i::I, surface_area::FT, n_updrafts::I) where {FT, I}
     a_s = surface_area/FT(n_updrafts)
-    c = Cloud{FT}(0,0,0)
+    c = Cloud{FT}(0,0,0,0)
     s_bc = SurfaceBC{FT}(0,0,0,0)
     surface_scalar_coeff = 1.7072226094205676
     # TODO: FIXME:
@@ -41,14 +41,14 @@ function compute_cloud_base_top_cover!(UpdVar, grid::Grid{FT}, q::StateVec, tmp:
   @inbounds for i in ud
     UpdVar[i].cloud.base = grid.zc[k_2]
     UpdVar[i].cloud.top = FT(0)
-    # UpdVar[i].cloud.updraft_top = FT(0)
+    UpdVar[i].cloud.updraft_top = FT(0)
     UpdVar[i].cloud.cover = FT(0)
     @inbounds for k in over_elems_real(grid)
       a_ik = q[:a, k, i]
       z_k = grid.zc[k]
-      # if a_ik > FT(1e-3)
-      #   UpdVar[i].cloud.updraft_top = max(UpdVar[i].cloud.updraft_top, z_k)
-      # end
+      if a_ik > FT(1e-3)
+        UpdVar[i].cloud.updraft_top = max(UpdVar[i].cloud.updraft_top, z_k)
+      end
       if tmp[:q_liq, k, i] > FT(1e-8) && a_ik > FT(1e-3)
         UpdVar[i].cloud.base  = min(UpdVar[i].cloud.base, z_k)
         UpdVar[i].cloud.top   = max(UpdVar[i].cloud.top, z_k)
