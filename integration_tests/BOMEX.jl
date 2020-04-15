eafe0a3cafd46cf186aa9c0024c5000d9f38a0d7
yairchn <yairchn@caltech.edu>
2020-04-15 10:45:09 -0700

    back to master

--- integration_tests/BOMEX.jl
+++ integration_tests/BOMEX.jl
@@ -19,7 +19,7 @@ mkpath(test_data_dir)
 # set `accept_new_solution = true`, and run once. Then,
 # reset to `false` before committing. This parameter should
 # never be `true` for commits.
-const accept_new_solution = false
+const accept_new_solution = true
 
 scms = SingleColumnModels
 @testset "Integration test: EDMF equations (BOMEX)" begin
